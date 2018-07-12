1254689a41af340582364c0610f20ac64bbdbb14
mariouvega <mario.vega014@gmail.com>
2018-07-12 15:11:33 -0400

    added add to do feature

--- src/App.js
+++ src/App.js
@@ -3,10 +3,70 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  addTodo(e) {
+    // the default behavior of a form is to submit and refresh the page, so we put e.preventDefault to prevent that from happening
+    e.preventDefault()
+    let name = this.refs.name.value
+    let completed = this.refs.completed.value
+    let counter = this.state.counter
+    let todo = {
+      name,
+      completed,
+      counter
+    }
+    counter+=1
+    let todos = this.state.todos
+
+    todos.push(todo)
+
+    this.setState({
+      todos: todos,
+      counter: counter
+    });
+
+    // we did this to reset the form after submission
+    this.refs.todoForm.reset()
+  }
+
+
+
+  constructor() {
+    // super(); => instantiating the base class component
+    super();
+    // the firts this is being referred to the base component thats why we are using .bind
+    this.addTodo = this.addTodo.bind(this)
+    this.state = {
+      todos: [],
+      title: 'React Simple Todo App',
+      counter: 0
+    }
+
+  }
+
+
+
+
+
   render() {
+    let title = this.state.title;
+    let todos = this.state.todos
+
+
+
+
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <h1>{title}</h1>
+        <form ref="todoForm" >
+          <input type="text" ref="name" placeholder="What do we need to do?" />
+          <input type="text" ref="completed" placeholder="Is it done?" />
+          <button onClick={this.addTodo}>Add</button>
+        </form>
+        <ul>
+          {todos.map((todo => <li key={todo.counter}>{todo.name} {todo.completed}</li>))}
+        </ul>
       </div>
     );
   }
