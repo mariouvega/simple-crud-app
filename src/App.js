19600136f6be74e6f348c23669ee2d99c9423193
mariouvega <mario.vega014@gmail.com>
2018-07-13 21:26:32 -0400

    added edit todo feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
 
+  editTodo(index) {
+    let todo = this.state.todos[index]
+    this.refs.name.value = todo.name
+    this.refs.completed.value = todo.completed
+
+    this.setState({
+      creating: 1,
+      index: index
+    })
+    this.refs.name.focus()
+  }
+
+
   removeTodo(index) {
     let todos = this.state.todos
-    let todo = todos.find(function(todo) {
-      return todo.counter === index
-    })
 
-    todos.splice(todo, 1)
+    todos.splice(index, 1)
 
     this.setState({
       todos: todos
@@ -24,25 +33,30 @@ class App extends Component {
     e.preventDefault()
     let name = this.refs.name.value
     let completed = this.refs.completed.value
-    let counter = this.state.counter
-    let todo = {
-      name,
-      completed,
-      counter
-    }
-    counter+=1
     let todos = this.state.todos
 
+    if(this.state.creating === 0) {
+      let todo = {
+        name,
+        completed,
+      }
+      todos.push(todo)
+    } else {
+      let i = this.state.index
+      todos[i].name = name
+      todos[i].completed = completed
+    }
+
     // this will add our todo object to our todos array
-    todos.push(todo)
 
     this.setState({
       todos: todos,
-      counter: counter
+      creating: 0
     });
 
     // we did this to reset the form after submission
     this.refs.todoForm.reset()
+    this.refs.name.focus()
   }
 
 
@@ -53,16 +67,18 @@ class App extends Component {
     // the firts this is being referred to the base component thats why we are using .bind
     this.addTodo = this.addTodo.bind(this)
     this.removeTodo = this.removeTodo.bind(this)
+    this.editTodo = this.editTodo.bind(this)
     this.state = {
       todos: [],
       title: 'React Simple Todo App',
-      counter: 0
+      creating: 0,
+      index: ''
     }
 
   }
 
   render() {
-    let title = this.state.title;
+    let title = this.state.title
     let todos = this.state.todos
 
     return (
@@ -74,7 +90,7 @@ class App extends Component {
           <button onClick={this.addTodo}>Add</button>
         </form>
         <ul>
-          {todos.map((todo => <li key={todo.counter}>{todo.name} {todo.completed} <button onClick={this.removeTodo.bind(null, todo.counter)}>DONE!</button></li> ))}
+          {todos.map((todo, index) => <li key={index}>{todo.name} - {todo.completed} <button onClick={this.editTodo.bind(null, index)}>Edit</button> <button onClick={this.removeTodo.bind(null, index)}>DONE!</button></li> )}
         </ul>
       </div>
     );
